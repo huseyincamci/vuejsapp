5d8052728984e9cf6daaddbc540692cd01f1878d
huseyincamci <hcamci.ce@gmail.com>
2017-04-12 13:07:01 +0300

    Created data & method

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: 'Huseyin'
+        name: 'Huseyin',
+        job: 'Student'
+    },
+    methods: {
+        greet: function(time){
+            return 'Good ' + time + ' ' + this.name;
+        }
     }
 });
