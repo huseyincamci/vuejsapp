641c3cc1eab97806abce98538d302d295b9fef0a
huseyincamci <hcamci.ce@gmail.com>
2017-04-13 12:01:19 +0300

    Events

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ new Vue({
     data: {
         name: 'Huseyin',
         job: 'Student',
+        age: 25,
         website: 'https://www.google.com',
-        htmlElement: '<strong>STRONG</strong>'
+        htmlElement: '<strong>STRONG</strong>',
+        x: 0,
+        y: 0
     },
     methods: {
         greet: function(time){
             return 'Good ' + time + ' ' + this.name;
+        },
+        add: function (year) {
+            this.age += year;
+        },
+        subtract: function (year) {
+            this.age -= year;
+        },
+        updateXY: function (event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
